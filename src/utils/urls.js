e0a79e748948d6329e13736f1366202523a489ae
SSOURABH58 <ssourabh025@gamil.com>
2023-03-26 00:58:50 +0530

    added server url 🔗

--- src/utils/urls.js
+++ src/utils/urls.js
@@ -1,1 +1,2 @@
-export const rootUrl = "https://github.com/SSOURABH58/convin/db.json"
+// export const rootUrl = "https://github.com/SSOURABH58/convin/db.json"
+export const rootUrl = "https://convinserver.onrender.com"
